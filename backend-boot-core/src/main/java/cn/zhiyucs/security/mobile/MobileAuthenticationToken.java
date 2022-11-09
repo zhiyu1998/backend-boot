6d01a7407ac246219e9d4516333b80885bf5097c
zhiyu1998 <542716863@qq.com>
2022-11-09 17:59:51 +0800

    🌈 style: 重构代码架构

--- backend-boot-core/src/main/java/cn/zhiyucs/security/mobile/MobileAuthenticationToken.java
+++ backend-boot-core/src/main/java/cn/zhiyucs/security/mobile/MobileAuthenticationToken.java
@@ -1,5 +1,6 @@
 package cn.zhiyucs.security.mobile;
 
+import lombok.EqualsAndHashCode;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.SpringSecurityCoreVersion;
@@ -12,6 +13,7 @@ import java.util.Collection;
  *
  * @author zhiyu1998
  */
+@EqualsAndHashCode(callSuper = true)
 public class MobileAuthenticationToken extends AbstractAuthenticationToken {
     private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
     private final transient Object principal;
