3b026a74217ddabc6c4e443911c1785a22855b2b
zhiyu1998 <542716863@qq.com>
2022-11-14 23:19:09 +0800

    🎈 perf: 解决依赖冲突

---
+++ backend-boot-api/src/main/java/cn/zhiyucs/api/test/TestApi.java
@@ -0,0 +1,14 @@
+package cn.zhiyucs.api.test;
+
+import cn.zhiyucs.common.utils.R;
+
+/**
+ * 测试接口
+ *
+ * @author zhiyu1998
+ * @since 2022/11/14 21:48
+ */
+public interface TestApi {
+
+    R<String> test();
+}
--- backend-boot-web/src/main/java/cn/zhiyucs/TestController.java
+++ backend-boot-web/src/main/java/cn/zhiyucs/TestController.java
@@ -1,11 +1,10 @@
 package cn.zhiyucs;
 
+import cn.zhiyucs.api.test.TestApi;
 import cn.zhiyucs.common.utils.R;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,12 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/10/29 15:16
  */
 @RestController
-@RequestMapping("new/test")
 @Tag(name = "新模块测试")
-@AllArgsConstructor
-public class TestController {
+public class TestController implements TestApi {
 
-    @GetMapping()
+    @Override
+    @GetMapping("/test")
     @Operation(summary = "测试接口")
     public R<String> test() {
         return R.ok("测试数据");
